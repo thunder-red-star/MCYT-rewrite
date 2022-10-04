6ecc1b75152dde9b1a1de4fac936603f8148a7d9
thundering redstar <wvongphanith30@gmail.com>
2022-10-04 13:24:54 +0000

    working status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,15 +1,22 @@
+import * as Discord from 'discord.js';
+
 export default async function ready (client) {
 	global.logger.info(`[${client.shard.ids[0] + 1}] Shard is ready`);
 	global.logger.info(`[${client.shard.ids[0] + 1}] Logged in as ${client.user.tag}`);
 
-	await client.user.setPresence({
-		activities: [{
-			name: 'getting rewritten',
-			type: 'PLAYING',
-		}, {
-			name: 'mc!help',
-			type: 'LISTENING',
-		}],
-		status: 'mobile'
+	client.user.setPresence({
+		activities: [
+			{
+				name: 'mc!help | /help',
+				type: Discord.ActivityType.Listening,
+			},
+			{
+				name: 'GitHub Copilot',
+				type: Discord.ActivityType.Watching,
+			},
+		],
+		status: 'online',
 	});
+
+	global.logger.info(`[${client.shard.ids[0] + 1}] Presence set`);
 }
