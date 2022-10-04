bb8c698ab4bb606d4463dda0478723549995e153
thundering redstar <wvongphanith30@gmail.com>
2022-10-04 01:28:36 +0000

    added logger module

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@
 
 import { ShardingManager } from 'discord.js';
 import { token } from './config/config.json';
+import Logger from './util/logger/index.cjs';
+import fs from 'fs';
+import path from 'path';
+
+// Dirname polyfill
+const __dirname = new URL('.', import.meta.url).pathname;
+
+global.logger = new Logger(path.join(__dirname, 'logs'));
 
 const manager = new ShardingManager('./bot.js', { token: token });
 
-manager.on('shardCreate', ()
+manager.on('shardCreate', (shard) => logger.info(`Launched shard ${shard.id}`));
